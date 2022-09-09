3c9f1d0fff5cc499b02be37c825e888ac48bcdba
JJhonsons <janis.jansons90@gmail.com>
2022-09-09 10:31:56 +0300

    atjauninajums 09.09.

--- Veselībasbaze2/script.js
+++ Veselībasbaze2/script.js
@@ -40,3 +40,37 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+//flipcar kods
+const cards = document.querySelectorAll(".cards__single");
+
+function flipCard() {
+  this.classList.toggle("flip");
+}
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+//slide show
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
--- class_29/script.js
+++ class_29/script.js
@@ -1,2 +1,27 @@
 let note = document.querySelector('.note');
 console.log(note.parentNode);
+
+let content = document.getElementById('menu');
+let firstChild = content.firstChild;
+console.log("...firstChild: ",firstChild);
+console.log("...firstChild.nodeType: ",firstChild.nodeType);
+console.log("...firstChild.nodeName: ",firstChild.nodeName);
+
+
+        let div = document.createElement('div');
+        div.id = 'content';
+        div.className = 'note';
+        div.innerHTML = '<p>CreateElement example</p>';
+        
+        document.body.appendChild(div);
+
+        let li = document.createElement('li');
+        li.textContent = 'Products';
+        menu.appendChild(li);
+        
+        li = document.createElement('li');
+        li.textContent = 'About Us';
+        
+        // select the ul menu element
+        const menu = document.querySelector('#menu');
+        menu.appendChild(li);
